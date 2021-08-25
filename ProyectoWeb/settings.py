1d57f4668285c2780aafc5061414de6ec7ba9953
PabloAndroid33 <78930713+PabloAndroid33@users.noreply.github.com>
2021-08-25 11:48:38 -0500

    Update settings.py

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -81,24 +81,24 @@ WSGI_APPLICATION = 'ProyectoWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tiendapedidos',
-        'USER':'postgres',
-        'PASSWORD':'pablo',
-        'HOST':'127.0.0.1',
-        'DATABASE_PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+#DATABASES = {
+#    'default': {
+ #       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+  #      'NAME': 'tiendapedidos',
+   #     'USER':'postgres',
+    #    'PASSWORD':'pablo',
+     #   'HOST':'127.0.0.1',
+      #  'DATABASE_PORT':'5432',
+    #}
+#}
 
 
 
